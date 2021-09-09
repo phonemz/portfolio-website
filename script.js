e2bcb089adb4f9d22b9929455ec93bb529c9f32d
phonemz <phonemyintzaw3033@gmail.com>
2021-09-10 01:49:29 +0630

    About page

---
+++ script.js
@@ -0,0 +1,16 @@
+
+document.querySelector("#moon").onclick = function(){
+    const element = document.body;
+    element.classList.toggle("dark-mode");
+    
+    const span = document.querySelector("#change-theme-text");
+    if (span.textContent === "the dark knight rises")
+    {
+        span.textContent= "let there be light"
+    }
+    else
+    {
+        span.textContent= "the dark knight rises"
+    }
+}
+
