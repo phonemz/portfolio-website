5671fe3ba3690a96e342f00fb6c2470d2419eed3
phonemz <phonemyintzaw3033@gmail.com>
2021-09-23 01:05:25 +0630

    mobile responsive done and drop unnecessary lines

--- contact-script.js
+++ contact-script.js
@@ -2,15 +2,7 @@ document.querySelector("#moon-about").onclick = function(){
     const element = document.body;
     element.classList.toggle("dark-mode");
     
-    const span = document.querySelector("#change-theme-text-about");
-    if (span.textContent === "the dark knight rises")
-    {
-        span.textContent= "let there be light"
-    }
-    else
-    {
-        span.textContent= "the dark knight rises"
-    }
+    
 }
 document.querySelector("#menu").onclick = function () {
     const page = document.querySelector(".container-contact");
