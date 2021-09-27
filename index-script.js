437c72288ac50322e3582e6f5ddcb49c76b1aba2
phonemz <phonemyintzaw3033@gmail.com>
2021-09-28 01:03:59 +0630

    netlify test

--- index-script.js
+++ index-script.js
@@ -3,6 +3,8 @@ document.querySelector("#moon").onclick = function () {
     const element = document.body;
     element.classList.toggle("dark-mode");
     
+    const moonicon = document.querySelector("#moon");
+    moonicon.style.filter = 'invert(100%)';
     
 }
 
